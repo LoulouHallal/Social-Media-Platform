d910ae73446f9dc1fd871fb6a4e4ed0cd7c04377
aliserhan315 <aliserhan315@gmail.com>
2025-04-08 00:27:38 +0300

    working on navbar and added tailwind css

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,37 @@
+import Link from 'next/link'
 import React from 'react'
+import MobileMenu from './MobileMenu'
+import Image from 'next/image'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between items-center h-24'>
+      
+      <div className=''/>
+      <Link href="/" className='font-bold text-xl text-blue-600'>Social</Link>
+      
+      <div className='hidden md:flex'>
+        <div className=''>
+          <Link href="/" className='flex gap-2'>
+          <Image src="/images/home.png" alt="home" width={17} height={17} className='cursor-pointer'/>
+          </Link>
+          <Link href="/" className='flex gap-2'>
+          <Image src="/images/friends.png" alt="friends" width={17} height={17} className='cursor-pointer'/>
+          </Link>
+          <Link href="/" className='flex gap-2'>
+          <Image src="/images/stories.png" alt="stories" width={17} height={17} className='cursor-pointer'/>
+          </Link>
+        </div>
+      </div>
+  
+<div className=''>
+      <MobileMenu/>
+      </div>
+
+
+
+
+    </div>
   )
 }
 
